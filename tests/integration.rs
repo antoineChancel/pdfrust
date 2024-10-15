e892fc259762b068afc67d3b77f1f87e04f441a3
achancel <achancel@scor.com>
2024-10-15 16:50:54 +0200

    update integration tests

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,20 +1,26 @@
 #[test]
 fn test_helloworld() {
     let file = std::fs::read("data/helloworld.pdf").unwrap();
-    pdfrust::trailer(&file);
-    // pdfrust::xref_table(&file);
+    let trailer = pdfrust::trailer(&file);
+    let xref_table = pdfrust::xref_table(&file);
+    let catalog_idx = xref_table.get(&trailer.root).unwrap();
+    let catalog = pdfrust::catalog(&file[*catalog_idx..]);
 }
 
 #[test]
 fn test_sample() {
     let file = std::fs::read("data/sample.pdf").unwrap();
-    // pdfrust::xref_table(&file);
-    // pdfrust::trailer(&file);
+    let trailer = pdfrust::trailer(&file);
+    let xref_table = pdfrust::xref_table(&file);
+    let catalog_idx = xref_table.get(&trailer.root).unwrap();
+    let catalog = pdfrust::catalog(&file[*catalog_idx..]);
 }
 
 #[test]
 fn test_tracemonkey() {
     let file = std::fs::read("data/tracemonkey.pdf").unwrap();
-    // pdfrust::xref_table(&file);
-    // pdfrust::trailer(&file);
+    let trailer = pdfrust::trailer(&file);
+    let xref_table = pdfrust::xref_table(&file);
+    let catalog_idx = xref_table.get(&trailer.root).unwrap();
+    let catalog = pdfrust::catalog(&file[*catalog_idx..]);
 }
