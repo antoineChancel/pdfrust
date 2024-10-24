6439524b8ae049f36e5ced1d53f0261d354a1727
achancel <achancel@scor.com>
2024-10-24 18:21:27 +0200

    refactoring integration tests

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,26 +1,21 @@
 #[test]
 fn test_helloworld() {
     let file = std::fs::read("data/helloworld.pdf").unwrap();
-    // pdfrust::trailer(&file);
     pdfrust::xref::xref_table(&file);
-    // let catalog_idx = xref_table.get(&trailer.root).unwrap();
-    // let catalog = pdfrust::catalog(&file[*catalog_idx..]);
+    let xref = pdfrust::xref::xref_table(&file);
+    pdfrust::trailer(&file, &xref);
 }
 
 #[test]
 fn test_sample() {
     let file = std::fs::read("data/sample.pdf").unwrap();
-    // pdfrust::trailer(&file);
-    pdfrust::xref::xref_table(&file);
-    // let catalog_idx = xref_table.get(&trailer.root).unwrap();
-    // let catalog = pdfrust::catalog(&file[*catalog_idx..]);
+    let xref = pdfrust::xref::xref_table(&file);
+    pdfrust::trailer(&file, &xref);
 }
 
 #[test]
 fn test_tracemonkey() {
     let file = std::fs::read("data/tracemonkey.pdf").unwrap();
-    // pdfrust::trailer(&file);
-    pdfrust::xref::xref_table(&file);
-    // let catalog_idx = xref_table.get(&trailer.root).unwrap();
-    // let catalog = pdfrust::catalog(&file[*catalog_idx..]);
+    let xref = pdfrust::xref::xref_table(&file);
+    pdfrust::trailer(&file, &xref);
 }
