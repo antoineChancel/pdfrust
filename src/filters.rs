189a4efd7130d409b1e2c0713587e0c5ca2bb0df
achancel <achancel@scor.com>
2024-10-25 13:12:15 +0200

    fix flate decode for sample.pdf

--- src/filters.rs
+++ src/filters.rs
@@ -4,12 +4,16 @@ use std::io::Read;
 pub fn flate_decode(bytes: &[u8]) -> String {
     println!("{:?}", bytes.len());
     let mut d = ZlibDecoder::new(bytes);
-    let mut s = String::new();
-    match d.read_to_string(&mut s) {
+    // 10 times the size of the compressed stream
+    let buf = &mut vec![0; bytes.len() * 10];
+    match d.read(buf) {
         Ok(_) => (),
         Err(e) => {
             panic!("Error: {:?}", e);
         }
     }
-    s
+    // sample.pdf contains non utf-8 characters in decompressed streams
+    String::from_utf8_lossy(buf).to_string()
 }
+
+
